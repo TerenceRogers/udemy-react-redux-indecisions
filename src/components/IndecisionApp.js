a761c33b2182796f9935241aa2ef15dd6765588a
Terence Rogers <trogers@tnlrogers.com>
2018-03-20 16:37:59 -0700

    Through Section 6, Lecture 57: Webpack Dev Server

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import AddOption from './AddOption';
+import Action from './Action';
+import Header from './Header';
+import Options from './Options';
+
+class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+    
+    this.state = {
+      options: [],
+    };
+  }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (error) {
+      // Do nothing with this error
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.options.length !== prevState.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+  handleDeleteOptions() {
+    this.setState(() => ({ options: [] }));
+  }
+  handleDeleteOption(option) {
+    this.setState((prevState) => ({
+      options: prevState.options.filter(o => o !== option)
+    }));
+  }
+  handlePick() {
+    const randomNum = Math.random();
+    const randomIndex = Math.floor(randomNum * this.state.options.length);
+    alert(`Selected: ${this.state.options[randomIndex]}`);
+  }
+  handleAddOption(option) {
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
+    }
+
+    this.setState((prevState) => ({ options: prevState.options.concat(option) }));
+  }
+  render() {
+    const title = "Indecision"
+    const subTitle = "Put your life in the hands of a computer"
+
+    return (
+      <div>
+        <Header subTitle={subTitle}/>
+        <Action 
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options 
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption 
+          handleAddOption={this.handleAddOption}
+        />
+      </div>
+    );
+  };
+};
+
+export default IndecisionApp;
